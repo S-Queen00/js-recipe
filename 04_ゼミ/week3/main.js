f4729589d6ef0846754e61def5b114f2aac86c5c
S-Queen <sake4951@gmail.com>
2021-10-15 20:06:30 +0900

    メモ配列（途中まで）

---
+++ 04_ゼミ/week3/main.js
@@ -0,0 +1,31 @@
+//配列 []の中に入れたものを並べられる
+//const aisatsu = ["おはよう", "こんにちは", "こんばんは"]
+//console.log(aisatsu)
+
+//.push=[]で配列を追加できる
+//aisatsu.push = ["おやすみ"]
+//console.log(aisatsu)
+
+const memoInput = document.getElementById("memo-input");
+const addButton = document.getElementById("add-button");
+const memoContainer = document.getElementById("memo-container");
+let memos = ["おはよう", "こんにちは", "こんばんは"];
+
+//console.dir(memoinput);
+
+addButton.onclick = function () {
+  const text = memoInput.value;
+  const card = document.createElement("div");
+  card.textContent = text;
+  memoContainer.append(card);
+
+  memos.push(memoInput.value);
+
+  updateMemoContainer()
+};
+
+const updateMemoContainer = function(){
+  memoContainer.innerHTML = ""
+}
+
+for ()
